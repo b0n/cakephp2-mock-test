a5958780f0f3786389c947d04d489ea12bb13e48
hrht461 <kousuketakeuchi@gmail.com>
2020-04-20 16:09:12 +0900

    new: mock の callback のテストを追加

--- Test/Case/MockTest.php
+++ Test/Case/MockTest.php
@@ -45,17 +45,7 @@ class Subject
      */
     public function doSomething()
     {
-        $this->notify('something');
-    }
-
-    /**
-     *
-     */
-    public function doSomethingBad()
-    {
-        foreach ($this->observers as $observer) {
-            $observer->reportError(42, 'Something bad happened', $this);
-        }
+        $this->notify('do something');
     }
 
     /**
@@ -65,6 +55,17 @@ class Subject
     {
         foreach ($this->observers as $observer) {
             $observer->update($argument);
+        }
+    }
+
+    /**
+     *
+     */
+    public function doSomethingBad()
+    {
+        foreach ($this->observers as $observer) {
+            /* @var Observer $observer */
+            $observer->reportError(42, 'Something bad happened', $this);
         }
     }
 }
@@ -96,5 +97,88 @@ class Observer
  */
 class MockTest extends CakeTestCase
 {
+    /**
+     * Subject::doSomethig() が Observer::update() を呼び出す引数が正しいかテスト
+     */
+    public function testObserversAreUpdated()
+    {
+        $observer = $this->getMock('Observer', array('update'));
+
+        $observer->expects($this->once())
+                 ->method('update')
+                 ->with($this->equalTo('do something'));
+
+        $subject = new Subject('My subject');
+        $subject->attach($observer);
+
+        $subject->doSomething();
+    }
+
+    /**
+     * Subject::doSomethigBad() が Observer::reportError() を呼び出す引数が正しいかそれぞれの引数についてテスト
+     */
+    public function testErrorReported()
+    {
+        $observer = $this->getMock('Observer', array('reportError'));
+        $observer->expects($this->once())
+                 ->method('reportError')
+                 ->with($this->greaterThan(0),
+                     $this->stringContains('Something'),
+                     $this->anything());
+
+        $observer2 = $this->getMock('Observer', array('reportError'));
+        $observer2->expects($this->once())
+                  ->method('reportError')
+                  ->with($this->equalTo(42),
+                      $this->stringStartsWith('Something'),
+                      $this->isInstanceOf('Subject'));
+
+        $subject = new Subject('My subject');
+        $subject->attach($observer);
+        $subject->attach($observer2);
+
+        $subject->doSomethingBad();
+    }
+
+    /**
+     * Subject::doSomethigBad() が Observer::reportError() を呼び出す引数が正しいかコールバック関数を用いてそれぞれの引数についてテスト
+     */
+    public function testErrorReportedCallback()
+    {
+        $observer = $this->getMock('Observer', array('reportError'));
+        $observer->expects($this->any())
+                 ->method('reportError')
+                 ->with($this->greaterThan(0),
+                     $this->stringContains('Something'),
+                     $this->callback(function ($subject) {
+                         return is_callable(array($subject, 'getName'))
+                                && get_class($subject) == 'Subject'
+                                && $subject->getName() == 'My own subject';
+                     }));
+
+        $observer2 = $this->getMock('Observer', array('reportError'));
+        $observer2->expects($this->any())
+                  ->method('reportError')
+                  ->with(
+                      $this->callback(function ($int) {
+                          return $int > 0
+                                 && $int < 43;
+                      }),
+                      $this->callback(function ($name) {
+                          return substr($name, 0, 1) == 'S'
+                                 && substr($name, -1, 1) == 'd';
+                      }),
+                      $this->callback(function ($subject) {
+                          return is_callable(array($subject, 'getName'))
+                                 && get_class($subject) == 'Subject'
+                                 && $subject->getName() == 'My own subject';
+                      }));
+
+        $subject = new Subject('My own subject');
+        $subject->attach($observer);
+
+        $subject->doSomethingBad();
+    }
+
 
 }
